50194627d6eeb1c57f70a34ff55c9f3ac71dcd0c
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-10-21 11:29:16 +0530

    refactor: update styles.css and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html", // Scans your main HTML file for classes.
-    "./src/**/*.{js,ts,jsx,tsx}", // Recursively scans all JavaScript, TypeScript, and JSX/TSX files inside the 'src' directory for classes.
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
