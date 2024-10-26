62862c1e55f09ea3bb741b8b7c98895e2c255fdd
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-10-26 08:38:33 +0530

    add vanilla js template

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,9 @@
 import eslintJS from "@eslint/js"
 import globals from "globals"
-import eslintReact from "eslint-plugin-react"
-import eslintReactHooks from "eslint-plugin-react-hooks"
-import eslintReactRefresh from "eslint-plugin-react-refresh"
-import eslintJsxA11y from "eslint-plugin-jsx-a11y"
 
 export default [
   {
-    files: ["**/*.{js,jsx}"],
+    files: ["**/*.js"],
     ignores: ["dist"],
 
     languageOptions: {
@@ -16,45 +12,15 @@ export default [
 
       parserOptions: {
         ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
         sourceType: "module",
       },
     },
 
-    settings: {
-      react: {
-        version: "18.3",
-      },
-    },
-
-    plugins: {
-      react: eslintReact,
-      "react-hooks": eslintReactHooks,
-      "react-refresh": eslintReactRefresh,
-      "jsx-a11y": eslintJsxA11y,
-    },
-
     rules: {
       ...eslintJS.configs.recommended.rules,
-      ...eslintReact.configs.recommended.rules,
-      ...eslintReact.configs["jsx-runtime"].rules,
-      ...eslintReactHooks.configs.recommended.rules,
-      ...eslintJsxA11y.configs.recommended.rules,
-
       "no-use-before-define": "error",
       "no-duplicate-imports": "error",
       camelcase: "error",
-
-      "react/jsx-no-target-blank": "off",
-      "react/prop-types": "off",
-
-      "jsx-a11y/anchor-is-valid": "off",
-      "jsx-a11y/prefer-tag-over-role": "error",
-
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
     },
   },
 ]
--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "/src/styles.css"
-
-export default function App() {
-  return (
-    <main>
-      <h1 className="text-3xl">Hello, World!</h1>
-    </main>
-  )
-}
