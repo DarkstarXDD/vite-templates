f0798d47f84a8f8f8868df1f8c1825ffa20c9532
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-11-06 09:49:43 +0530

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,68 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss"
+
 export default {
   content: ["./index.html", "./src/**/*"],
   theme: {
-    extend: {},
+    screens: {
+      md: "58rem",
+    },
+
+    fontFamily: {
+      body: ["Roboto", "sans-serif"],
+    },
+
+    fontSize: {
+      "heading-desktop": [
+        "3.5rem",
+        { lineHeight: "1", letterSpacing: "0px", fontWeight: "700" },
+      ],
+
+      "heading-mobile": [
+        "2.5rem",
+        { lineHeight: "1", letterSpacing: "0px", fontWeight: "700" },
+      ],
+
+      body: [
+        "1rem",
+        { lineHeight: "1.5", letterSpacing: "0px", fontWeight: "400" },
+      ],
+
+      "body-bold": [
+        "1rem",
+        { lineHeight: "1.5", letterSpacing: "0px", fontWeight: "700" },
+      ],
+
+      "body-small": [
+        "0.75rem",
+        { lineHeight: "1.5", letterSpacing: "0px", fontWeight: "700" },
+      ],
+    },
+
+    colors: {
+      transparent: "transparent",
+      white: "hsl(0, 0%, 100%)",
+      grey: "hsl(236, 7%, 48%)",
+      vermellion: "hsl(4, 100%, 67%)",
+      navy: {
+        pale: "hsl(235, 18%, 26%)",
+        dark: "hsl(234, 29%, 20%)",
+      },
+      gradient: {
+        start: "hsl(15, 100%, 61%)",
+        end: "hsl(346, 100%, 66%)",
+      },
+    },
+
+    extend: {
+      gridTemplateColumns: {
+        "3fr-2fr": "3fr 2fr",
+        list: "auto 1fr",
+      },
+    },
   },
   plugins: [],
-  corePlugins: { preflight: true }, // Make false to remove the tailwind reset
-}
+
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+} satisfies Config
